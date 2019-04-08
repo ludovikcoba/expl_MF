1f44ab2085e2dbe095b9a9583cf2438d76dbc74e
Ludovik Çoba <lcoba@unishk.edu.al>
2019-04-08 14:11:33 +0200

    eval ndcg

--- src/evalRec.R
+++ src/evalRec.R
@@ -1,30 +1,64 @@
 evalRec <- function(rec, test, topN = 3, positiveThreshold = 3){
-  browser()
+
   test <- test %>% filter(score >= positiveThreshold)
   
   nrUsr <- length(unique(test$user))
   
+  Hits <- semi_join(rec, test, by = c("user", "item")) 
+  
   #### Precision
-  TPcount <- semi_join(rec, test, by = c("user", "item")) %>% 
+  
+  
+  TPcount <- Hits %>% 
     group_by(user) %>%
     summarise(TP = n())
   FPcount <- anti_join(rec, test, by = c("user", "item")) %>% 
     group_by(user) %>%
     summarise(FP = n())
   
-  prec <- full_join(TPcount, FPcount)
-  prec[is.na(prec)] <- 0
+  precUsr <- full_join(TPcount, FPcount)
+  precUsr[is.na(precUsr)] <- 0
   
-  prec <- prec %>% mutate(precision = TP/(TP + FP))
+  precUsr <- precUsr %>% mutate(precision = TP/(TP + FP))
   
   # mean precision
-  precision <- sum(prec$precision)/nrUsr
+  precision <- sum(precUsr$precision)/nrUsr
 
   #### nDCG
+
+  nDCGusr <- Hits %>% 
+    select(user,rank) %>%
+    group_by(user) %>%
+    nest() %>%
+    mutate(val = map2(data, topN, eval_nDCG)) %>% 
+    select(user, val) %>% unnest()
   
-  
+  # mean nDCG
+  nDCG <- sum(nDCGusr$val) / nrUsr
   
 }
 
+eval_nDCG <- function(rank, topN){
+  
+  idcg <- getiDCG(topN)
+  rank <- rank$rank
+  if(1 %in% as.vector(rank)){
+    dcg <- 1/log2(rank[-1])
+    dcg <- 1 + sum(dcg)
+  }else{
+    dcg <- sum(1/log2(rank))
+  }
+  
+  dcg/idcg
+}
 
-
+getiDCG <- function(n){
+  
+  idcg <- 1
+  
+  if(n > 1){
+    idcg <- idcg + sum(1/log2(2:n))
+  }
+  
+  idcg
+}
